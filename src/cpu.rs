fba627e5115814009c20e198f70d8980fabff46c
darosan2003 <darosan2003@gmail.com>
2023-07-04 17:14:09 +0200

    [ADDED] Added opcodes [26/34]

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,7 @@
+extern crate rand;
+
+use rand::Rng;
+
 use std::fs::File;
 use std::io::Read;
 
@@ -52,6 +56,8 @@ pub trait Cpu {
 
 pub struct Chip8 {
     pub v:      [u8; MAX_REG],
+    pub dt:     u8,
+    pub st:     u8,
     pub i:      usize,
     pub pc:     usize,
     pub sp:     usize,
@@ -61,7 +67,7 @@ pub struct Chip8 {
 
 impl Cpu for Chip8 {
     fn new() -> Self {
-        Chip8 { v: [0;MAX_REG], i: 0, pc: 0x200, sp: 0, mem: [0;MAX_MEMORY], stack: [0;MAX_STACK] }
+        Chip8 { v: [0;MAX_REG], dt: 0, st: 0, i: 0, pc: 0x200, sp: 0, mem: [0;MAX_MEMORY], stack: [0;MAX_STACK] }
     }
 
     fn load_rom(&mut self, rom: &String) {
@@ -134,29 +140,39 @@ impl Cpu for Chip8 {
         match instruction {
             Opcode::Clear => println!("CLS"),
 
-            Opcode::Return => if self.sp > 0 {
-                self.pc = self.stack[self.sp] as usize;
-                self.sp -= 1;
+            Opcode::Return => {
+                if self.sp > 0 {
+                    self.pc = self.stack[self.sp] as usize;
+                    self.sp -= 1;
+                }
             },
 
             Opcode::JumpAddr => self.pc = (opcode & 0x0FFF) as usize,
 
-            Opcode::Call => if self.sp < MAX_STACK {
-                self.sp += 1;
-                self.stack[self.sp] = self.pc as u16;
-                self.pc = (opcode & 0x0FFF) as usize
+            Opcode::Call => {
+                if self.sp < MAX_STACK {
+                    self.sp += 1;
+                    self.stack[self.sp] = self.pc as u16;
+                    self.pc = (opcode & 0x0FFF) as usize
+                }
             },
 
-            Opcode::SkipEqualVxkk => if self.v[(opcode >> 8 & 0x0F) as usize] == (opcode & 0x00FF) as u8 {
-                self.pc = if self.pc + 2 >= MAX_MEMORY { 0x200 } else { self.pc + 2 };
+            Opcode::SkipEqualVxkk => {
+                if self.v[(opcode >> 8 & 0x0F) as usize] == (opcode & 0x00FF) as u8 {
+                    self.pc = if self.pc + 2 >= MAX_MEMORY { 0x200 } else { self.pc + 2 };
+                }
             } 
             
-            Opcode::SkipNotEqualVxkk => if self.v[(opcode >> 8 & 0x0F) as usize] != (opcode & 0x00FF) as u8 {
-                self.pc = if self.pc + 2 >= MAX_MEMORY { 0x200 } else { self.pc + 2 };
+            Opcode::SkipNotEqualVxkk => {
+                if self.v[(opcode >> 8 & 0x0F) as usize] != (opcode & 0x00FF) as u8 {
+                    self.pc = if self.pc + 2 >= MAX_MEMORY { 0x200 } else { self.pc + 2 };
+                }
             },
 
-            Opcode::SkipEqualVxVy => if self.v[(opcode >> 8 & 0x0F) as usize] == self.v[(opcode >> 4 & 0x00F) as usize] {
-                self.pc = if self.pc + 2 >= MAX_MEMORY { 0x200 } else { self.pc + 2 };     
+            Opcode::SkipEqualVxVy => {
+                if self.v[(opcode >> 8 & 0x0F) as usize] == self.v[(opcode >> 4 & 0x00F) as usize] {
+                    self.pc = if self.pc + 2 >= MAX_MEMORY { 0x200 } else { self.pc + 2 };     
+                }
             },
 
             Opcode::LoadVxkk => self.v[(opcode >> 8 & 0x0F) as usize] = (opcode & 0x00FF) as u8,
@@ -194,24 +210,56 @@ impl Cpu for Chip8 {
                 self.v[(opcode >> 8 & 0x0F) as usize] >>= 1;
             },
 
-            Opcode::SubtractNotBorrow => println!("SUBN Vx, Vy"),
-            Opcode::LeftShift => println!("SHL Vx, Vy"),
+            Opcode::SubtractNotBorrow => {
+                if self.v[(opcode >> 4 & 0x00F) as usize] > self.v[(opcode >> 8 & 0x0F) as usize] {
+                    self.v[0x0F] = 1;
+                }else {
+                    self.v[0x0F] = 0;
+                }
+                self.v[(opcode >> 8 & 0x0F) as usize] = self.v[(opcode >> 4 & 0x00F) as usize] - self.v[(opcode >> 8 & 0x0F) as usize];
+            },
+            
+            Opcode::LeftShift => {
+                self.v[0x0F] = if self.v[(opcode >> 8 & 0x0F) as usize] & 0x80 != 0 { 1 } else { 0 };
+                self.v[(opcode >> 8 & 0x0F) as usize] <<= 1;
+            },
+            
             Opcode::SkipNotEqualVxVy => println!("SNE Vx, Vy"),
-            Opcode::LoadI => println!("LD I, addr"),
-            Opcode::JumpAddrV0 => println!("JP V0, addr"),
-            Opcode::Random => println!("RND Vx, byte"),
+
+            Opcode::LoadI => self.i = (opcode & 0x0FFF) as usize,
+
+            Opcode::JumpAddrV0 => self.pc = (opcode & 0x0FFF) as usize + self.v[0x00] as usize,
+            
+            Opcode::Random => self.v[(opcode >> 8 & 0x0F) as usize] = rand::thread_rng().gen_range(0..=255) & ((opcode & 0x00FF) as u8),
+            
             Opcode::Draw => println!("DRW Vx, Vy, nibble"),
             Opcode::SkipIfVxPressed => println!("SKP Vx"),
             Opcode::SkipIfVxNotPressed => println!("SKNP Vx"),
-            Opcode::LoadVxDelayTimer => println!("LD Vx, DT"),
+
+            Opcode::LoadVxDelayTimer => self.v[(opcode >> 8 & 0x0F) as usize] = self.dt,
+
             Opcode::LoadPressedKeyVx => println!("LD Vx, K"),
-            Opcode::LoadDelayTimerVx => println!("LD DT, Vx"),
-            Opcode::LoadSoundTimerVx => println!("LD ST, Vx"),
-            Opcode::AddIVx => println!("ADD I, Vx"),
+
+            Opcode::LoadDelayTimerVx => self.dt = self.v[(opcode >> 8 & 0x0F) as usize],
+
+            Opcode::LoadSoundTimerVx => self.st = self.v[(opcode >> 8 & 0x0F) as usize],
+            
+            Opcode::AddIVx => self.i += self.v[(opcode >> 8 & 0x0F) as usize] as usize,
+
             Opcode::LoadISpritePositionVx => println!("LD F, Vx"),
             Opcode::LoadIBCDVx => println!("LD B, Vx"),
-            Opcode::LoadIVRegisters => println!("LD [I], Vx"),
-            Opcode::LoadVRegistersI => println!("LD Vx, [I]")
+
+            Opcode::LoadIVRegisters => {
+                for pos in 0..(opcode >> 8 & 0x0F) as usize {
+                    self.mem[self.i + pos] = self.v[pos];
+                }
+            },
+
+            Opcode::LoadVRegistersI => {
+                for pos in 0..(opcode >> 8 & 0x0F) as usize {
+                    self.v[pos] = self.mem[self.i + pos];
+                }
+            }
         }
     }
 }
